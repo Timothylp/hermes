ba9fd1bb508abfb85fcc106fa72666d3286f501b
Damien <damien.noel@univ-brest.fr>
2022-11-21 20:36:42 +0100

    correction mauvais merge

--- components/Conversation.tsx
+++ components/Conversation.tsx
@@ -4,13 +4,32 @@ import Message from "./Message";
 
 function Conversation() {
   const [messages, setMessages] = useState<MessageInterface[]>([
-    { userId: "a", content: "coucou ", isMine: true },
-    { userId: "a", content: "coucou ", isMine: true },
-    { userId: "a", content: "coucou ", isMine: true },
+    {
+      userId: "a",
+      content: "J'adore sucer des grosses bites",
+      isMine: false,
+    },
+    {
+      userId: "a",
+      content:
+        "Je pense qu'Alan tout-compte fait est quelqu'un de super sympa et intéressant, je pense l'inviter au resto pour avoir une super conversation avec lui ",
+      isMine: false,
+    },
+    { userId: "a", content: "Monsieur Babau le sang", isMine: false },
+    { userId: "a", content: "T'es bizarre mec", isMine: true },
+    {
+      userId: "a",
+      content:
+        "Un petit oiseau se balladait au delà de la montage. Celui-ci avait très froid à cause des vents forts",
+      isMine: true,
+    },
   ]);
 
   return (
-    <div className="h-screen w-screen flex-col justify-center">
+    <div
+      id="conversation"
+      className=" flex h-screen w-screen flex-col gap-5 py-3"
+    >
       {messages.map((message) => (
         <Message {...message} />
       ))}
--- components/Message.tsx
+++ components/Message.tsx
@@ -1,22 +1,33 @@
 import { MessageInterface } from "../model/interfaces/message-interface";
-import Image from 'next/image'
+import Image from "next/image";
 
 function Message(message: MessageInterface) {
-    const tempUrl =
+  const tempUrl =
     "https://scontent-cdt1-1.xx.fbcdn.net/v/t1.6435-9/139243499_3945354685495166_9112770101454389337_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=H96yA52Cg-oAX_fGxVc&_nc_ht=scontent-cdt1-1.xx&oh=00_AfAMiHD0aztiu3xauvmM_tj66oWlPTPsqNttAxgp25GF5g&oe=63A21A30";
-  return (<div className = "w-1/2 h-300 flex">
-    {/* <div className = "flex-1">
-        <icon
-    </div> */}
-
-    <Image
+  return (
+    <div
+      id="message"
+      className={`flex h-auto w-full items-end gap-1 px-3 
+        ${message.isMine ? "flex-row-reverse" : ""}`}
+    >
+      <Image
+        id="message__pp"
         src={tempUrl}
         alt="profile"
         width={256}
         height={171}
-        className=" flex-1 md:h-18 md:w-18 h-12 w-12 rounded-full object-cover"
-    />
-    <div className = "flex-5"></div>
-  </div>);
+        className={`mb-0 mb-1 h-6 w-6 rounded-full md:h-16 md:w-16
+        ${message.isMine ? "hidden" : ""}`}
+      />
+      <p
+        id="message__content"
+        className={` w-auto rounded-md p-2  text-xs font-medium	text-white	 ${
+          message.isMine ? "bg-indigo-800" : "bg-blue-600"
+        }`}
+      >
+        {message.content}
+      </p>
+    </div>
+  );
 }
 export default Message;
