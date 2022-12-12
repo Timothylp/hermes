f7563f8f46388bd8b4e89dee7b3b102a5b22edaf
Naedim <dnoeldev@gmail.com>
2022-12-12 20:12:56 +0100

    petite refacto api conversations

--- components/Conversation.tsx
+++ components/Conversation.tsx
@@ -12,7 +12,8 @@ function Conversation({
   selectedConversation: ConversationProfilInterface;
   setSelectedConversation: any;
 }) {
-  const [conversationContent, setConversationContent] = useState<ConversationContentInterface>();
+  const [conversationContent, setConversationContent] =
+    useState<ConversationContentInterface>();
 
   useEffect(() => {
     fetch(`/api/conversation`)
@@ -22,27 +23,6 @@ function Conversation({
         setConversationContent(conv);
       });
   }, [selectedConversation]);
-
-  // const [messages, setMessages] = useState<MessageInterface[]>([
-  //   {
-  //     userId: "a",
-  //     content: "J'adore sucer des grosses bites",
-  //     isMine: false,
-  //   },
-  //   {
-  //     userId: "a",
-  //     content:
-  //       "Je pense qu'Alan tout-compte fait est quelqu'un de super sympa et intéressant, je pense l'inviter au resto pour avoir une super conversation avec lui ",
-  //     isMine: false,
-  //   },
-  //   { userId: "a", content: "Monsieur Babau le sang", isMine: false },
-  //   { userId: "a", content: "T'es bizarre mec", isMine: true },
-  //   {
-  //     userId: "a",
-  //     content: "Un petit oiseau se balladait au delà de la montage. Celui-ci avait très froid à cause des vents forts",
-  //     isMine: true,
-  //   },
-  // ]);
 
   const handleCloseConversation = () => {
     setSelectedConversation(null);
--- pages/api/conversation.ts
+++ pages/api/conversation.ts
@@ -7,38 +7,48 @@ type GetResponse = {
   conversation: ConversationContentInterface;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<GetResponse>) {
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<GetResponse>
+) {
   if (req.method === "GET") {
-    let messages: MessageInterface[] = [
-      {
-        userId: "a",
-        content: "J'adore sucer des grosses bites",
-        isMine: false,
-      },
-      {
-        userId: "a",
-        content:
-          "Je pense qu'Alan tout-compte fait est quelqu'un de super sympa et intéressant, je pense l'inviter au resto pour avoir une super conversation avec lui ",
-        isMine: false,
-      },
-      { userId: "a", content: "Monsieur Babau le sang", isMine: false },
-      { userId: "a", content: "T'es bizarre mec", isMine: true },
-      {
-        userId: "a",
-        content: "Un petit oiseau se balladait au delà de la montage. Celui-ci avait très froid à cause des vents forts",
-        isMine: true,
-      },
-    ];
-
-    let conversation: ConversationContentInterface = {
-      messages: messages,
-      members: [
-        { name: "Timothy", surname: "Le Pallec" },
-        { name: "Damien", surname: "Noël" },
-      ],
+    let retrievedConversation: GetResponse = {
+      conversation: getConversation(),
     };
-
-    let retrievedConversation: GetResponse = { conversation: conversation };
     res.status(200).json(retrievedConversation);
   }
+}
+
+function getConversation(): ConversationContentInterface {
+  let messages: MessageInterface[] = [
+    {
+      userId: "a",
+      content: "J'adore sucer des grosses bites",
+      isMine: false,
+    },
+    {
+      userId: "a",
+      content:
+        "Je pense qu'Alan tout-compte fait est quelqu'un de super sympa et intéressant, je pense l'inviter au resto pour avoir une super conversation avec lui ",
+      isMine: false,
+    },
+    { userId: "a", content: "Monsieur Babau le sang", isMine: false },
+    { userId: "a", content: "T'es bizarre mec", isMine: true },
+    {
+      userId: "a",
+      content:
+        "Un petit oiseau se balladait au delà de la montage. Celui-ci avait très froid à cause des vents forts",
+      isMine: true,
+    },
+  ];
+
+  let conversation: ConversationContentInterface = {
+    messages: messages,
+    members: [
+      { name: "Timothy", surname: "Le Pallec" },
+      { name: "Damien", surname: "Noël" },
+    ],
+  };
+
+  return conversation;
 }
